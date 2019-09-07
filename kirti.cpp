dca211919c9cea374ad76894756bf47f95f217d2
kirti <kirtideswal9@gmail.com>
2019-09-07 12:26:40 +0530

    first change

---
+++ kirti.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()  {
+
+    int a=10;
+
+    count<<"Learning git and github";
+    count<<"Basic of git and github";
+    count<<a;
+
+    //This is our first version.
+    return0;
+}
